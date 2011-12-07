a2eeefd4bf8dc59ca3e1f13b296de60bd2f18660
GrimReaper6464 <GrimReaper6464@45b86d19-1226-07b7-177f-2ae735336182>
2011-12-07 19:49:38 +0000

    Much better names

---
+++ Combined Code/PredatorPrey/PredatorPrey/PredatorPrey/Fluffies.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace PredatorPrey
+{
+    class Fluffies : Creature
+    {
+
+        public Fluffies(Vector2 position) : base(position)
+        {
+
+        }
+
+        //public override void update(VisionContainer vc)
+        //{
+            // step1: update values that change with time (hunger)
+
+            // step2: use prey rules (extract data from VisionContainer) to create a list of movement vectors
+
+            // step3: sum up movement vectors using stored weights
+
+            // step4: update velocity, position, and direction
+        //}
+
+        public void updateWeights()
+        {
+            // step1: calculate fitness of current state
+
+            // step2: classify state as either good or not good
+
+            // step3: if state is good do nothing
+            //        if state is not good, attribute the bad state to 1 (or more) of the rules
+            //              ???????
+        }
+
+        //this is just my first idea, feel free to change it if you think of something
+        //also the weights are just temporary values right now
+        public override double calculateFitness()
+        {
+            double numberOfWolves = 0;
+            double closestWolf = 0;
+            return Parameters.hungerWeight * base.hunger + Parameters.numberOfWolvesWeight * numberOfWolves + Parameters.closestWolfWeight * closestWolf;
+        }
+    }
+}
---
+++ Combined Code/PredatorPrey/PredatorPrey/PredatorPrey/Wulffies.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace PredatorPrey
+{
+    class Wulffies : Creature
+    {
+
+        public Wulffies(Vector2 position) : base(position)
+        {
+
+        }
+
+        //public override void update(VisionContainer vc)
+        //{
+            // step1: update values that change with time (hunger)
+
+            // step2: use predator rules (extract data from VisionContainer) to create a list of movement vectors
+
+            // step3: sum up movement vectors using stored weights
+
+            // step4: update velocity, position, and direction
+        //}
+
+        public void updateWeights()
+        {
+            // step1: calculate fitness of current state
+
+            // step2: classify state as either good or not good
+
+            // step3: if state is good do nothing
+            //        if state is not good, attribute the bad state to 1 (or more) of the rules
+            //              ???????
+        }
+
+        public int calculateFitness()
+        {
+            // step1: ???????
+
+            return 0;
+        }
+    }
+}
