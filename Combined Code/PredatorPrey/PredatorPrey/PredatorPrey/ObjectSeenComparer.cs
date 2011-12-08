3c08fd5b7084b94a33179983cddd664cb53bd87e
moomoocalf <moomoocalf@45b86d19-1226-07b7-177f-2ae735336182>
2011-12-08 21:29:19 +0000

    Fixed glitch with SortedList.  Using an ObjectSeenComparer to sort List

---
+++ Combined Code/PredatorPrey/PredatorPrey/PredatorPrey/ObjectSeenComparer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace PredatorPrey
+{
+    class ObjectSeenComparer : IComparer<ObjectSeen>
+    {
+        Vector2 viewerPos;
+
+        public ObjectSeenComparer(Vector2 viewerPos)
+        {
+            this.viewerPos = viewerPos;
+        }
+
+        int IComparer<ObjectSeen>.Compare(ObjectSeen ob1, ObjectSeen ob2)
+        {
+            float distance1 = Vector2.Subtract(ob1.position, viewerPos).Length();
+            float distance2 = Vector2.Subtract(ob2.position, viewerPos).Length();
+
+            if (distance1 < distance2)
+                return 1;
+            else if (distance1 > distance2)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
